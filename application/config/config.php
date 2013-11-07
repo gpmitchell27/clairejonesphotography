f7767da4ed85640a79adf9fe066276139c4c1486
gpmitchell27 <gpmitchell27@fullsail.edu>
2013-11-07 16:39:06 -0500

    j

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= './clairejonesphotography/';
+$config['base_url']	= 'http://gpmitchell27.github.io/clairejonesphotography/';
 
 /*
 |--------------------------------------------------------------------------
